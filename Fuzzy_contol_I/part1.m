9e2cb50f3094c046f7560b69ade1677d3fa1cccf
laurapm <l.perezm@edu.uah.es>
2019-11-30 19:49:57 +0000

    fuzzy pussy control

---
+++ Fuzzy_contol_I/part1.m
@@ -0,0 +1,28 @@
+%  PRACTICE 3. FUZZY CONTROL
+% The aim of the practice is strudying fuzzy control design in order to 
+% control robots which are in move.
+% The practice will be divide in two parts:
+        % PART 1. Designing a fuzzy control to control the position 
+        % PART 2. Designing a fuzzy control to avoid abstacles
+% LINES THAT ALWAYS HAVE TO BE: 
+% clear variables, 
+% close opened windows 
+% and clean the comand windows
+%-------------------------------------------------------------------------%
+clear variables;
+close all;
+clc;
+%-------------------------------------------------------------------------%
+%Tiempo de muestreo
+Ts=100e-3
+% Referencia x-y de posicion
+refx=10*rand-5;
+refy=10*rand-5;
+% Ejecutar Simulacion
+sim('PositionControl.slx')
+% Mostrar
+x=salida_x.signals.values;
+y=salida_y.signals.values;
+figure;plot(x,y);
+grid on;
+hold on;
