8b8c9d3c102c5c0f6b00e7f959f9c4daca9dcc8e
laurapm <l.perezm@edu.uah.es>
2019-12-02 11:37:50 +0000

    han pasado cosas que creo que no funcionan pero quien sabe

--- Fuzzy_contol_I/part1.m
+++ Fuzzy_contol_I/part1.m
@@ -16,13 +16,19 @@ clc;
 %Tiempo de muestreo
 Ts=100e-3
 % Referencia x-y de posicion
-refx=10*rand-5;
-refy=10*rand-5;
-% Ejecutar Simulacion
-sim('PositionControl.slx')
-% Mostrar
-x=salida_x.signals.values;
-y=salida_y.signals.values;
-figure;plot(x,y);
-grid on;
-hold on;
+x_0 = 0;
+y_0 = 0;
+th_0 = 0;
+for c = 1:10
+    refx=10*rand-5;
+    refy=10*rand-5;
+    % Ejecutar Simulacion
+    sim('PositionControl.slx')
+    % Mostrar
+    x=salida_x.signals.values;
+    y=salida_y.signals.values;
+    figure;
+    plot(x,y);
+    grid on;
+    hold on;
+end
